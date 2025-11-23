bdee63039e14a2180b460d222322eaec04c12bac
vishwa raja <vishwamj180@gmail.com>
2025-11-23 14:55:39 +0000

    in assert folder i wange image called 2025-11-21 use this has a logo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
           <div className="flex flex-col gap-4 md:col-span-4">
             <Link href="/" className="flex items-center gap-2">
               <Image 
-                src="/static/media/logo.png" 
+                src="/static/media/2025-11-21.png" 
                 alt="VV Software Developer Logo"
                 width={40} 
                 height={40}
